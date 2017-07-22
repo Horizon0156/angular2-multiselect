5102d5a854f9275531445f839a1b65cbc3feb227
Stefan Wilkes <stefan.wilkes@gmail.com>
2017-07-22 11:57:50 +0200

    Removed unrequired template files. Added Demo- and  Multiselect Moduel

--- src/demo/demo.module.ts
+++ src/demo/demo.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { DemoComponent } from './demo.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    DemoComponent
   ],
   imports: [
     BrowserModule,
@@ -15,6 +15,6 @@ import { AppComponent } from './app.component';
     HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [DemoComponent]
 })
-export class AppModule { }
+export class DemoModule { }
